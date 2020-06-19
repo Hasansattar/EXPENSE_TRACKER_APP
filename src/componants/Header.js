4b10623b3acaa6439482d5419bc6d41dcc31f775
hasansattar <hasansattar650@gmail.com>
2020-06-20 03:30:10 +0500

    tracker app

---
+++ src/componants/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header =() => {
+  return (
+    <div className="bgWhite">
+      <h1>
+        Expense Manager <br />
+        
+      </h1>
+    </div>
+  )
+}
